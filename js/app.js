a88211696aa5dc9856a3886d0136a1548da20c0d
Luis Francisco <183943582+luisfrancisco2b@users.noreply.github.com.>
2026-07-02 16:48:37 -0300

    feat: connect to Awesome API and successfully retrieve exchange rates

--- js/app.js
+++ js/app.js
@@ -8,17 +8,54 @@ const resultText = document.getElementById("result-text");
 // Base URL for the currency exchange API
 const baseUrl = "https://economia.awesomeapi.com.br/last/";
 
-// Simple function to test the button click and element
-convertBtn.addEventListener("click", () => {
+// Function to fetch exchange rate and calculate conversion
+async function convertCurrency() {
   const amount = amountInput.value;
   const from = fromCurrency.value;
   const to = toCurrency.value;
 
-  // Log to current values to the console
-  console.log(
-    `Testing click ! Amount:$ ${amount}, | From: ${from} | To: ${to}`,
-  );
+  // Validation to check if the user entered a value
+  if (!amount || amount <= 0) {
+    resultText.innerText = "Por favor, insira um valor válido!";
+    return;
+  }
 
-  // Tempory message on screen to confirm it works
-  resultText.textContent = `Connecting... Converting ${amount} ${from} to ${to}!`;
-});
+  // Validation to check if the currencies are the same
+  if (from === to) {
+    resultText.innerText = "Por favor, selecione moadas diferentes!";
+    return;
+  }
+
+  resultText.innerText = "Buscando taxas de câmbio... ";
+
+  try {
+    // Constructuring the dynamic URL. For example: USD -> BRL
+    const response = await fetch(`${baseUrl}${from}-${to}`);
+
+    // Checking if there was a response from API
+    if (!response.ok) {
+      throw new Error("Moeadas não suportadas pela API!");
+    }
+
+    // Transforming the response in JSON
+    const data = await response.json();
+
+    // Creating the dynamic key. For example: "USDBRL"
+    const currencyPair = `${from}${to}`;
+
+    // Extracting the bid (exchange rate) from the response
+    const exchangeRate = data[currencyPair].bid;
+
+    console.log("Dados recebidos da API: ", data);
+    console.log("Taxa de câmbio atual: ", exchangeRate);
+
+    // Temporary display to verify
+    resultText.innerText = `1 ${from} = ${exchangeRate} ${to}`;
+  } catch (error) {
+    console.error("Erro ao buscar dados: ", error);
+    resultText.innerText =
+      "Erro ao buscar dados na API! Tente novamente mais tarde.";
+  }
+}
+// Listening to the button click event
+convertBtn.addEventListener("click", convertCurrency);
